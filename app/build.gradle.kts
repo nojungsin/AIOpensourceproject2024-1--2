034f7343fbe4387e6ce9eb7a24860403aaae5802
hyejun <127915444+nojungsin@users.noreply.github.com>
2024-04-05 09:34:21 +0900

    2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.appprac2"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.appprac2"
--- app/src/main/java/com/example/appprac2/MainActivity.java
+++ app/src/main/java/com/example/appprac2/MainActivity.java
@@ -2,13 +2,75 @@ package com.example.appprac2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView text1, text2;
+    CheckBox chkAgree;
+    RadioGroup rGroup;
+    RadioButton rdoDog, rdoCat, rdoRabbit;
+    Button btnOK;
+    ImageView imgPet;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("애완동물 사진 보기");
+
+        text1 = (TextView) findViewById(R.id.Text1);
+        chkAgree = (CheckBox) findViewById(R.id.ChkAgree);
+
+        text2 = (TextView) findViewById(R.id.Text2);
+        rGroup = (RadioGroup) findViewById(R.id.Rgroup);
+        rdoDog = (RadioButton) findViewById(R.id.RdoDog);
+        rdoCat = (RadioButton) findViewById(R.id.RdoCat);
+        rdoRabbit = (RadioButton) findViewById(R.id.RdoRabbit);
+
+        btnOK = (Button) findViewById(R.id.BtnOK);
+        imgPet = (ImageView) findViewById(R.id.ImgPet);
+
+        chkAgree.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (chkAgree.isChecked() == true){
+                    text2.setVisibility(View.VISIBLE);
+                    rGroup.setVisibility(View.VISIBLE);
+                    btnOK.setVisibility(View.VISIBLE);
+                    imgPet.setVisibility(View.VISIBLE);
+                }
+                else{
+                    text2.setVisibility(View.INVISIBLE);
+                    rGroup.setVisibility(View.INVISIBLE);
+                    btnOK.setVisibility(View.INVISIBLE);
+                    imgPet.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+        btnOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int checkedRadio = rGroup.getCheckedRadioButtonId();
+                if (checkedRadio == R.id.RdoDog)
+                    imgPet.setImageResource(R.drawable.dog);
+                else if (checkedRadio == R.id.RdoCat)
+                    imgPet.setImageResource(R.drawable.cat);
+                else if (checkedRadio == R.id.RdoRabbit)
+                    imgPet.setImageResource(R.drawable.rabbit);
+                else Toast.makeText(getApplicationContext(), "동물 먼저 선택하세요", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
